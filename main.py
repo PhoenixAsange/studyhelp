e8f52c98ad6a4dfb766ad68e8198057072343fa1
PhoenixAsange <phoenixxasange@gmail.com>
2025-02-10 15:41:25 +1000

    added directory selection functionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from ctypes import wstring_at
+
 from pptx import Presentation
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
-
-filePath = input("Enter file path (single = file, multiple = directory): ")
+import os
 
 template = """"
     You are a highly intelligent and helpful study assistant. You help students by answering questions based on the provided course material.
@@ -28,13 +29,13 @@ def extractText(filePath):
         for shape in slide.shapes:
             if hasattr(shape, "text"):
                 text.append(shape.text)
-    return "\n".join(text)
+    text = "\n".join(text)
+    print(f"\nExtracted Text (Preview):\n {text[:50]}...")
+    return text
 
 def modelConversation():
     context = ""
     print("Type 'exit' to exit conversation.")
-    # result = chain.invoke({"context": content, "question": question})
-    # context += f"\nCourse Content: {content}\nAI: {result}"
     while True:
         print("Bot: What would you like help with?")
         userInput = input("You: ")
@@ -45,12 +46,22 @@ def modelConversation():
         print("Bot:", result)
         context += f"\nUser: {userInput}\nAI: {result}"
 
-
-try:
-    content = extractText(filePath)
-    print("\nExtracted Text:\n", content[:50])
-except:
-    print("Error")
+while True:
+    filePath = input("Enter file path (single = file, multiple = directory): ")
+    try:
+        if os.path.isdir(filePath):
+            content = []
+            fileAmount = 0
+            for file in os.listdir(filePath):
+                fullPath = os.path.join(filePath, file)
+                content.append(extractText(fullPath))
+                fileAmount += 1
+            print(f"{fileAmount} files have been parsed")
+        else:
+            content = extractText(filePath)
+        break
+    except Exception as e:
+        print(f"Error: {e}. Please ensure the directory/file path is correct.")
 
 if __name__ == "__main__":
     modelConversation()
